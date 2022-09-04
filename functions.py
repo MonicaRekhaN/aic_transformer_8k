e5a949ed117737637ac48efd4b26a02b69ac75df
MonicaRekhaN <107790881+MonicaRekhaN@users.noreply.github.com>
2022-09-04 10:34:12 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -131,11 +131,11 @@ class Decoder(tf.keras.layers.Layer):
     x = self.dropout(x, training=training)
 
     for i in range(self.num_layers):
-      x, block1, block2 = self.dec_layers[i](x, enc_output, training,
+      x, block1 = self.dec_layers[i](x, enc_output, training,
                                              look_ahead_mask, padding_mask)
       
       attention_weights['decoder_layer{}_block1'.format(i+1)] = block1
-      attention_weights['decoder_layer{}_block2'.format(i+1)] = block2
+      #attention_weights['decoder_layer{}_block2'.format(i+1)] = block2
     
     # x.shape == (batch_size, target_seq_len, d_model)
     return x, attention_weights
@@ -185,16 +185,16 @@ class DecoderLayer(tf.keras.layers.Layer):
   def __init__(self, d_model, num_heads, dff, rate=0.1):
     super(DecoderLayer, self).__init__()
 
-    self.mha1 = MultiHeadAttention(d_model, num_heads)
-    self.mha2 = MultiHeadAttention(d_model, num_heads)
+    #self.mha1 = MultiHeadAttention_imp(d_model, num_heads)
+    self.mha2 = MultiHeadAttention_imp(d_model, num_heads)
 
     self.ffn = point_wise_feed_forward_network(d_model, dff)
  
-    self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+    #self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
     self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
     self.layernorm3 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
     
-    self.dropout1 = tf.keras.layers.Dropout(rate)
+    #self.dropout1 = tf.keras.layers.Dropout(rate)
     self.dropout2 = tf.keras.layers.Dropout(rate)
     self.dropout3 = tf.keras.layers.Dropout(rate)
     
@@ -204,9 +204,10 @@ class DecoderLayer(tf.keras.layers.Layer):
     # enc_output.shape == (batch_size, input_seq_len, d_model)
 
     # using look ahead mask so that during self attention current query dont consider future token
-    attn1, attn_weights_block1 = self.mha1(x, x, x, look_ahead_mask)  # (batch_size, target_seq_len, d_model)
-    attn1 = self.dropout1(attn1, training=training)
-    out1 = self.layernorm1(attn1 + x)
+    #attn1, attn_weights_block1 = self.mha1(x, x, x, look_ahead_mask)  # (batch_size, target_seq_len, d_model)
+    #attn1 = self.dropout1(attn1, training=training)
+    #out1 = self.layernorm1(attn1 + x)
+    out1 = x
     
     # use padding mask to avoid padded values of both enc_output and dec_input
     attn2, attn_weights_block2 = self.mha2(
@@ -218,7 +219,7 @@ class DecoderLayer(tf.keras.layers.Layer):
     ffn_output = self.dropout3(ffn_output, training=training)
     out3 = self.layernorm3(ffn_output + out2)  # (batch_size, target_seq_len, d_model)
     
-    return out3, attn_weights_block1, attn_weights_block2
+    return out3, attn_weights_block2
 
 """ Within this block the Multihead attention layer is built to get weights for q,k,v 
  The output of MHA is passed to FFN and normalization"""
@@ -226,7 +227,7 @@ class EncoderLayer(tf.keras.layers.Layer):
   def __init__(self, d_model, num_heads, dff, rate=0.1):
     super(EncoderLayer, self).__init__()
 
-    self.mha = MultiHeadAttention(d_model, num_heads)
+    self.mha = MultiHeadAttention_imp(d_model, num_heads)
     self.ffn = point_wise_feed_forward_network(d_model, dff)
 
     self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
@@ -256,9 +257,9 @@ def point_wise_feed_forward_network(d_model, dff):
 
 """ This class build Multihead attention layer with number of heads passed ( 8 as per paper) and builds
 Weigth for q - Query , k - Key , v - Value  . Weights matrix will be as many as number of heads """
-class MultiHeadAttention(tf.keras.layers.Layer):
+class MultiHeadAttention_imp(tf.keras.layers.Layer):
   def __init__(self, d_model, num_heads):
-    super(MultiHeadAttention, self).__init__()
+    super(MultiHeadAttention_imp, self).__init__()
     self.num_heads = num_heads
     self.d_model = d_model
     
@@ -386,6 +387,8 @@ def load_image(image_path):
 # from globals import initialize
 def evaluate(image):
   # global tokenizer,image_features_extract_model,transformer
+  global tokenizer,image_features_extract_model,transformer,output,dec_mask
+  global start_token,decoder_input,end_token
   temp_input = tf.expand_dims(load_image(image)[0], 0)
   img_tensor_val = image_features_extract_model(temp_input)
   img_tensor_val = tf.reshape(img_tensor_val, (img_tensor_val.shape[0], -1, img_tensor_val.shape[3]))
